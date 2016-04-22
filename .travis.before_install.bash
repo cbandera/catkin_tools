d84928f9bbfb57b20ea58ee4ca8dfa9046cb9555
Jonathan Bohren <jbo@jhu.edu>
2016-04-22 18:00:54 -0400

    travis: fixing enchant problem

--- .travis.before_install.bash
+++ .travis.before_install.bash
@@ -15,7 +15,7 @@ elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
 
   # Install CMake if necessary
   if ! hash cmake 2>/dev/null; then brew install cmake; fi
-  brew install enchant
+  #brew install enchant
 
   virtualenv -p $PYTHON venv
   source venv/bin/activate
